8cda8cd28a6a518cb8fdee45f350c155694651f1
pearmini <pearmini@users.noreply.github.com>
2024-07-10 07:29:51 +0000

    deploy: f14ee7c8842064dc0c3bf9d7e6f667969c3b9ed8

--- _npm/@antv/g@6.0.9/_esm.js
+++
@@ -1,7 +0,0 @@
-/**
- * Bundled by jsDelivr using Rollup v2.79.1 and Terser v5.19.2.
- * Original file: /npm/@antv/g@6.0.9/dist/index.esm.js
- *
- * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
- */
-export*from"../g-camera-api@2.0.8/_esm.js";export*from"../g-dom-mutation-observer-api@2.0.5/_esm.js";export*from"../g-lite@2.0.5/_esm.js";export*from"../g-web-animations-api@2.0.6/_esm.js";export default null;
